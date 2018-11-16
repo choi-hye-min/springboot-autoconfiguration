5927049737dc7df185a3d27dc54391bd34068f54
arthur <gogoke1@naver.com>
2018-11-17 00:11:42 +0900

    asdasdas

--- src/main/java/com/arthur/arthurspirngbootstarter/HolomanReunner.java
+++ src/main/java/com/arthur/arthurspirngbootstarter/HolomanReunner.java
@@ -14,6 +14,6 @@ public class HolomanReunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println(holoman);
-        
+
     }
 }
